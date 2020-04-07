0c13949edc12bba665e51fffe3d1ff77caa33a00
Catalin Torge <torgeadelin@gmail.com>
2020-04-07 18:20:37 +0100

    Updates

---
+++ precache-manifest.46201654541210f16ec047c1b04c1fb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c3971fbf5c5cb2b0b12564ffb7f8a9",
+    "url": "/cycle/index.html"
+  },
+  {
+    "revision": "fa6c32750731505a1cd7",
+    "url": "/cycle/static/css/main.6815585d.chunk.css"
+  },
+  {
+    "revision": "2925f0cff31f393058dd",
+    "url": "/cycle/static/js/2.f442dd1d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/cycle/static/js/2.f442dd1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6c32750731505a1cd7",
+    "url": "/cycle/static/js/main.7d469866.chunk.js"
+  },
+  {
+    "revision": "1cb782fe8edbcc997b18",
+    "url": "/cycle/static/js/runtime-main.306fdb1b.js"
+  },
+  {
+    "revision": "bd5c342bce26c0a86c8963ea331e3c23",
+    "url": "/cycle/static/media/bike.bd5c342b.svg"
+  },
+  {
+    "revision": "49640d22c2f28458f1c8e3fa27a17eec",
+    "url": "/cycle/static/media/cycles.49640d22.csv"
+  },
+  {
+    "revision": "3ab25c66f40117daa75cc2f24dc78c8b",
+    "url": "/cycle/static/media/loading.3ab25c66.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ff140297d89ef8663765eb17d4971b8.js"
+  "/cycle/precache-manifest.46201654541210f16ec047c1b04c1fb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cycle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
